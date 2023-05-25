76442822cb9f0a5ef894268b8eed96bb0b0dbc5f
m-contour <m-contour@qq.com>
2023-05-25 23:20:02 +0800

    feat: finish lab3
    ditto

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -2,6 +2,7 @@
 #include "tcp_config.hh"
 
 #include <random>
+#include <iostream>
 
 using namespace std;
 
@@ -12,42 +13,121 @@ TCPSender::TCPSender(uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn)
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
-    // Your code here.
-    return {};
+    return outstanding_seqno_;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
-    // Your code here.
-    return {};
+    return consecutive_retransmission_times_;
 }
 
 optional<TCPSenderMessage> TCPSender::maybe_send()
 {
-    // Your code here.
-    return {};
+    if (segments_out_.size() > 0 && set_syn_) {
+        TCPSenderMessage segment = segments_out_.front();
+        segments_out_.pop();
+        return segment;
+    }
+
+    return nullopt;
 }
 
 void TCPSender::push(Reader &outbound_stream)
 {
-    // Your code here.
-    (void)outbound_stream;
+    uint64_t curr_window_size = window_size_ ? window_size_ : 1;
+    while (curr_window_size > outstanding_seqno_) {
+        TCPSenderMessage msg;
+
+        if (!set_syn_) {
+            msg.SYN = true;
+            set_syn_ = true;
+        }
+
+        msg.seqno = get_next_seqno();
+        uint64_t payload_size =
+            min(TCPConfig::MAX_PAYLOAD_SIZE, curr_window_size - outstanding_seqno_ - msg.SYN);
+        std::string payload = std::string(outbound_stream.peek()).substr(0, payload_size);
+        outbound_stream.pop(payload_size);
+
+        if (!set_fin_ && outbound_stream.is_finished() && payload.size() + outstanding_seqno_ + msg.SYN < curr_window_size) {
+            msg.FIN = true;
+            set_fin_ = true;
+        }
+
+        msg.payload = Buffer(std::move(payload));
+
+        // no data, stop sending
+        if (msg.sequence_length() == 0) {
+            break;
+        }
+
+        // no outstanding segments, restart timer
+        if (outstanding_seg_.empty()) {
+            RTO_timeout_ = initial_RTO_ms_;
+            timer_ = 0;
+        }
+
+        segments_out_.push(msg);
+
+        outstanding_seqno_ += msg.sequence_length();
+        outstanding_seg_.insert(std::make_pair(next_abs_seqno_, msg));
+        next_abs_seqno_ += msg.sequence_length();
+
+        if (msg.FIN) {
+            break;
+        }
+    }
 }
 
 TCPSenderMessage TCPSender::send_empty_message() const
 {
-    // Your code here.
-    return {};
+    TCPSenderMessage segment;
+    segment.seqno = get_next_seqno();
+
+    return segment; 
 }
 
 void TCPSender::receive(const TCPReceiverMessage &msg)
 {
-    // Your code here.
-    (void)msg;
+    if (!msg.ackno.has_value()) {
+        ; // Don't return directly
+    } else {
+        uint64_t recv_abs_seqno = msg.ackno.value().unwrap(isn_, next_abs_seqno_);
+        if (recv_abs_seqno > next_abs_seqno_) {
+            // Impossible, we couldn't transmit future data
+            return;
+        }
+
+        for (auto iter = outstanding_seg_.begin(); iter != outstanding_seg_.end();) {
+            const auto &[abs_seqno, segment] = *iter;
+            if (abs_seqno + segment.sequence_length() <= recv_abs_seqno) {
+                outstanding_seqno_ -= segment.sequence_length();
+                iter = outstanding_seg_.erase(iter);
+                // reset RTO and if outstanding data is not empty, start timer
+                RTO_timeout_ = initial_RTO_ms_;
+                if (outstanding_seg_.size() > 0) {
+                    timer_ = 0;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+    window_size_ = msg.window_size;
+    consecutive_retransmission_times_ = 0;
 }
 
 void TCPSender::tick(const size_t ms_since_last_tick)
 {
-    // Your code here.
-    (void)ms_since_last_tick;
+    timer_ += ms_since_last_tick;
+    auto iter = outstanding_seg_.begin();
+    if (timer_ >= RTO_timeout_ && iter != outstanding_seg_.end()) {
+        const auto &[abs_seqno, segment] = *iter;
+        if (window_size_ > 0) {
+            RTO_timeout_ *= 2;
+        }
+        timer_ = 0;
+        consecutive_retransmission_times_++;
+        segments_out_.push(segment);
+    }
 }
