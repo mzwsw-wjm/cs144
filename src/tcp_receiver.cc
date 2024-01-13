1fc82347eceb582493ca8fab16719df943206822
wjm0 <2838765836@qq.com>
2024-01-13 23:12:43 +0800

    lab2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,42 +1,31 @@
 #include "tcp_receiver.hh"
+#include "tcp_receiver_message.hh"
+#include "wrapping_integers.hh"
+#include <cstdint>
+#include <optional>
 
 using namespace std;
 
 void TCPReceiver::receive(TCPSenderMessage message, Reassembler &reassembler, Writer &inbound_stream)
 {
-    if (!set_syn_) {
-        if (!message.SYN) {
-            return; // drop all data if SYN isn't received
-        }
-        isn_ = message.seqno; // FIN occupied one seqno
+    if(!set_syn_) {
+        if(!message.SYN) { return; }
         set_syn_ = true;
+        isn_ = message.seqno;
+        reassembler.insert(message.seqno.unwrap(isn_, inbound_stream.bytes_pushed()+1), message.payload.release(), message.FIN, inbound_stream);
     }
-
-    const uint64_t checkpoint = inbound_stream.bytes_pushed() + 1;
-    const uint64_t abs_seqno = message.seqno.unwrap(isn_, checkpoint);
-    // unwrap function starts from isn_, which occupies one seqno.
-    // We calculate one index more so we need to minus it.
-    // But if SYN exists in this message, compensation is needed.
-    const uint64_t stream_index = abs_seqno - 1 + message.SYN;
-    reassembler.insert(stream_index, message.payload.release(), message.FIN, inbound_stream);
+    if(set_syn_ && !message.SYN) {
+        reassembler.insert(message.seqno.unwrap(isn_, inbound_stream.bytes_pushed()+1)-1, message.payload.release(), message.FIN, inbound_stream);
+    }
 }
 
 TCPReceiverMessage TCPReceiver::send(const Writer &inbound_stream) const
 {
-    TCPReceiverMessage recv_msg {};
-
-    const uint16_t window_size
-        = inbound_stream.available_capacity() > UINT16_MAX ? UINT16_MAX : inbound_stream.available_capacity();
-    if (!set_syn_) {
-        return {std::optional<Wrap32> {}, window_size};
-    }
-    // add one ISN(SYN) length
-    uint64_t abs_ackno_offset = inbound_stream.bytes_pushed() + 1;
-    if (inbound_stream.is_closed()) {
-        abs_ackno_offset++; // add one FIN
-    }
-    recv_msg.ackno = isn_ + abs_ackno_offset;
-    recv_msg.window_size = window_size;
-
-    return recv_msg;
+    TCPReceiverMessage ret{};
+    auto num = inbound_stream.bytes_pushed()+1;
+    if(inbound_stream.is_closed()) { num++; }
+    if(!set_syn_){ ret.ackno = nullopt; }
+    else { ret.ackno = make_optional<Wrap32>(Wrap32::wrap(num, isn_)); }
+    ret.window_size = (inbound_stream.available_capacity() < UINT16_MAX) ? inbound_stream.available_capacity() : UINT16_MAX;
+    return ret;
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,22 +1,29 @@
 #include "wrapping_integers.hh"
+#include <algorithm>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap(uint64_t n /* absolute seqno */, Wrap32 zero_point /* ISN */)
 {
-    return zero_point + static_cast<uint32_t>(n);
+    //Convert absolute seqno → seqno.
+    return Wrap32 { zero_point + static_cast<uint32_t>(n) };
 }
 
 uint64_t Wrap32::unwrap(Wrap32 zero_point, uint64_t checkpoint) const
 {
-    const uint32_t seqno_offset = this->raw_value_ - zero_point.raw_value_;
-    /* checkpoint > offset we just need to find the nearest one */
-    if (checkpoint > seqno_offset) {
-        constexpr uint64_t UINT32_SIZE = 1L << 32;
-        const uint64_t abs_seqno_extra_part_offset = checkpoint - seqno_offset + (UINT32_SIZE >> 1);
-        const uint64_t UINT32_SIZE_num = abs_seqno_extra_part_offset / UINT32_SIZE;
-        return UINT32_SIZE_num * UINT32_SIZE + seqno_offset;
+    //Convert seqno → absolute seqno.
+    auto seq = static_cast<uint64_t>(raw_value_ - zero_point.raw_value_);
+    const uint64_t offset = static_cast<uint64_t>(1) << 32;
+    if(checkpoint <= seq) { return seq; }
+    uint64_t p_checkpoint = checkpoint / offset;
+    seq = seq + p_checkpoint * offset;
+    if(checkpoint <= seq) {
+        auto up_seq = seq - offset;
+        if(checkpoint - up_seq < seq - checkpoint) { seq = up_seq; }
     }
-
-    return seqno_offset;
+    else {
+        auto up_seq = seq + offset;
+        if(up_seq > seq && up_seq-checkpoint < checkpoint-seq) { seq = up_seq; }
+    }
+    return seq;
 }
